d4acdeaeac313db8205bdfbd2854c0e16df204dc
leatherpants <sunxuhan1008@gmail.com>
2024-10-06 16:57:47 +1000

    replace background-image

--- src/components/CheckList.tsx
+++ src/components/CheckList.tsx
@@ -8,7 +8,7 @@ export default function CheckList() {
 
   const checklist = useSelector((state: RootState) => state.checklist);
   const dispatch = useDispatch();
-
+  const test = '';
   function onButtonClick() {
     dispatch(clearAll());
   }
